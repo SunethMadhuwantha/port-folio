58ea28f92ccc99f0fb8f066f4799cd22d8465829
Suneth <94039078+SunethMadhuwantha@users.noreply.github.com>
2025-02-07 23:07:15 +0530

    add photo

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -9,7 +9,7 @@ const Photo = () => {
         <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition:{delay:2,duration:0.4,ease:"easeIn"},}}>
             {/*image*/}
             <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition:{delay:2.4,duration:0.4,ease:"easeOut"},}} className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"> 
-                <Image src="/assests/work/photo1.png" priority quality={100} fill alt="" className="object-contain"/>
+                <Image src="/assests/work/photo2.png" priority quality={100} fill alt="" className="object-contain"/>
             </motion.div>
 
             {/* circle*/}
