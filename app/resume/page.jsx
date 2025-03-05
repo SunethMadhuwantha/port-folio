1ee75d85a6ac7db80ad16a6c341fb837f9c0e3cf
Suneth <94039078+SunethMadhuwantha@users.noreply.github.com>
2025-03-05 13:40:45 +0530

    create experience tab and details

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -36,7 +36,7 @@ const about = {
 //expirience
 const expirience = {
   icon:"/assets/resume/badge.svg",
-  title:"My expirience",
+  title:"My experience",
   description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   items:[
     {
@@ -145,10 +145,12 @@ const skills = {
     
 }
 
-import {Tabs,TabContent,TabList,TabsTrigger} from "@/components/ui/tabs"
+
 import {Tooltip,TooltipTrigger,TooltipContent,TooltipProvider} from "@/components/ui/tooltip"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { motion } from "framer-motion"
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+
 
 
 const Resume = () => {
@@ -166,17 +168,47 @@ const Resume = () => {
         defaultValue="experience"
         className="flex flex-col xl:flex-row gap-[60px]"
         >
-          <TabList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
             <TabsTrigger value="experience">Experience</TabsTrigger>
             <TabsTrigger value="education">Education</TabsTrigger>
             <TabsTrigger value="skills">Skills</TabsTrigger>
             <TabsTrigger value="about">About me</TabsTrigger>
-          </TabList>
+          </TabsList>
 
           <div className="min-h-[70vh] w-full">
-            <TabContent value="experience" className="w-full">
-              experience
-            </TabContent>
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{expirience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{expirience.description}</p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                      {expirience.items.map((item,index)=>{
+                        return(
+                          <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start">
+                            <span className="text-accent">{item.duration}</span>
+                            <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
+                            <div className="flex items-center gap-3">
+                              <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                              <p className="text-white/60">{item.company}</p>
+                            </div>
+
+                          </li>
+                        )
+                      })}
+                  </ul>
+
+                </ScrollArea>
+              </div>
+            </TabsContent>
+            <TabsContent value="education" className="w-full">
+              education
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              about
+            </TabsContent>
 
           </div>
 
