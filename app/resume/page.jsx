d4a8c180e88cd863f1221124e2b62ddf22a8b53d
Suneth <94039078+SunethMadhuwantha@users.noreply.github.com>
2025-03-07 11:23:01 +0530

    spelling changes

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -7,34 +7,34 @@ const about = {
   description:'lorem ipsum dolor sit amet, consectetur adip non pro',
   info:[
     {
-    fileName:"Name",
+    fieldName:"Name",
     fieldValue:"Luke colman"
   },
   {
-    fileName:"Phone",
+    fieldName:"Phone",
     fieldValue:"(+94) 71 3737 040"
   },
   {
-    fileName:"Expirence",
+    fieldName:"Experience",
     fieldValue:"1+ Years"
   },
   {
-    fileName:"Nationality",
+    fieldName:"Nationality",
     fieldValue:"Sri Lankan"
   },
   {
-    fileName:"Insta",
+    fieldName:"Insta",
     fieldValue:"kitty.007"
   },
   {
-    fileName:"Email",
+    fieldName:"Email",
     fieldValue:"90zuneth99@gmail.com"
   },
 ]
 }
 
-//expirience
-const expirience = {
+//experience
+const experience = {
   icon:"/assets/resume/badge.svg",
   title:"My experience",
   description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -80,25 +80,25 @@ const education = {
   description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   items:[
     {
-      instittution:"ABC company",
+      institution:"ABC company",
       degree:"Senior Developer",
       duration:"2019-2021",
       description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
-      instittution:"ABC company",
+      institution:"ABC company",
       degree:"Senior Developer",
       duration:"2019-2021",
       description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
-      instittution:"ABC company",
+      institution:"ABC company",
       degree:"Senior Developer",
       duration:"2019-2021",
       description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
-      instittution:"ABC company",
+      institution:"ABC company",
       degree:"Senior Developer",
       duration:"2019-2021",
       description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -146,6 +146,10 @@ const skills = {
           icon:<FaFigma/>,
           name:"figma",
         },
+        {
+          icon:<FaFigma/>,
+          name:"figma",
+        },
     
   ]
     
@@ -181,14 +185,14 @@ const Resume = () => {
             <TabsTrigger value="about">About me</TabsTrigger>
           </TabsList>
 
-          <div className="min-h-[70vh] w-full">
+          <div className=" w-full">
             <TabsContent value="experience" className="w-full">
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
-                <h3 className="text-4xl font-bold">{expirience.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{expirience.description}</p>
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
                 <ScrollArea className="h-[400px]">
                   <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
-                      {expirience.items.map((item,index)=>{
+                      {experience.items.map((item,index)=>{
                         return(
                           <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start">
                             <span className="text-accent">{item.duration}</span>
@@ -219,7 +223,7 @@ const Resume = () => {
                             <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.degree}</h3>
                             <div className="flex items-center gap-3">
                               <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
-                              <p className="text-white/60">{item.instittution}</p>
+                              <p className="text-white/60">{item.institution}</p>
                             </div>
 
                           </li>
@@ -230,42 +234,55 @@ const Resume = () => {
                 </ScrollArea>
               </div>
             </TabsContent>
-            <TabsContent value="skills" className="w-full h-full">
-              <div className="flex flex-col gap-[30px]">
-                 <div className=" flex flex-col gap-[30px] text-center xl:text-left">
-                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+            <TabsContent value="skills" className="w-full">
+  <div className="flex flex-col gap-[30px] text-center xl:text-left">
+    <h3 className="text-4xl font-bold">{skills.title}</h3>
+    <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+      {skills.description}
+    </p>
+    <ScrollArea className="h-[400px]">
+      <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+        {skills.skillList.map((skill,index)=>{
+          return(
+            <li key={index} >
+              <TooltipProvider delayDuration={100}>
+                <Tooltip>
+                  <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                    <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                      {skill.icon}
+                    </div>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className="capitalize">{skill.name}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            </li>
+          )
+        })}
+      </ul>
+    </ScrollArea>
+  </div>
+</TabsContent>
+            <TabsContent value="about" className="w-full text-center xl:text-left">
+                <div className="flex flex-col gap-[30px]">
+                  <h3 className="text-4xl font-bold">{about.title}</h3>
                   <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {skills.description}
+                  {about.description}
                   </p>
-                 </div>
-                 <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
-                  {skills.skillList.map((skill,index)=>{
-                    return(
-                      <li key={index} >
-                        <TooltipProvider delayDuration={100}>
-                          <Tooltip>
-                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
-                              <div className="text-6xl group-hover:text-accent transition-all duration-300">
-                                {skill.icon}
-
-                              </div>
-
-                            </TooltipTrigger>
-                            <TooltipContent>
-                              <p className="capitalize">{skill.name}</p>
-                            </TooltipContent>
-                          </Tooltip>
-                          
-                        </TooltipProvider>
-                        
-                      </li>
-                    )
-                  })}
-                 </ul>
-              </div>
-            </TabsContent>
-            <TabsContent value="about" className="w-full">
-              about
+                  <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                    {about.info.map((item,index)=>{
+                      return(
+                        <li key={index} className="flex items-center justify-center xl:justify-start gap-4" >
+                          <span className="text-white/60">{item.fieldName}</span>
+                          <span className="text-xl">{item.fieldValue}</span>
+                         
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              
             </TabsContent>
 
           </div>
