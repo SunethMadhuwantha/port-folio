ac8103f85415b0a00b2e9267b14d011c1ff19c68
Suneth <94039078+SunethMadhuwantha@users.noreply.github.com>
2025-03-11 14:29:43 +0530

    import dependencies to work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,18 @@
+"use client";
+
+import React,{ useState } from "react";
+import { motion } from "framer-motion";
+
+import {Swiper,SwiperSlide} from "swiper/react";
+import "swiper/css";
+
+import { BsArrow90DegRight,BsGithub } from "react-icons/bs";
+
+
+import { Tooltip,TooltipProvider,TooltipContent,TooltipTrigger  } from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
 
 
 const work = () => {
