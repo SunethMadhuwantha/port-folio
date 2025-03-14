e0ede5b25b2d02ac01c4791c4b5d30d675488971
Suneth <94039078+SunethMadhuwantha@users.noreply.github.com>
2025-03-14 13:51:25 +0530

    add project details

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -14,6 +14,37 @@ import { Tooltip,TooltipProvider,TooltipContent,TooltipTrigger  } from "@/compon
 import Link from "next/link";
 import Image from "next/image";
 
+const projects=[
+  {
+    num:'01',
+    catogory:"frontend",
+    title:'project 1',
+    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique quos ratione vero adipisci! Nihil at cum amet earum sequi molestiae voluptate sed voluptates laboriosam aliquam. Voluptate commodi archite",
+    stack:[{name:"Next.js"},{name:"tailwind.css"},{name:"HTML5"}],
+    image:"/assets/work/thumb1.png",
+    live:"",
+    github:"",
+  },
+  {
+    num:'02',
+    catogory:"frontend",
+    title:'project 2',
+    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique quos ratione vero adipisci! Nihil at cum amet earum sequi molestiae voluptate sed voluptates laboriosam aliquam. Voluptate commodi archite",
+    stack:[{name:"Next.js"},{name:"tailwind.css"}],
+    image:"/assets/work/thumb2.png",
+    live:"",
+    github:"",
+  },{
+    num:'03',
+    catogory:"frontend",
+    title:'project 3',
+    description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique quos ratione vero adipisci! Nihil at cum amet earum sequi molestiae voluptate sed voluptates laboriosam aliquam. Voluptate commodi archite",
+    stack:[{name:"Next.js"},{name:"tailwind.css"}],
+    image:"/assets/work/thumb3.png",
+    live:"",
+    github:"",
+  }
+]
 
 const work = () => {
   return (
