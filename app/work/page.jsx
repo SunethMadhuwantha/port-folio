282f9ef19b56bba32a9363a8a4a559cad8caa4ef
Suneth <94039078+SunethMadhuwantha@users.noreply.github.com>
2025-03-14 15:30:06 +0530

    display projects and set slider and images

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,12 +1,13 @@
 "use client";
 
+
 import React,{ useState } from "react";
 import { motion } from "framer-motion";
 
 import {Swiper,SwiperSlide} from "swiper/react";
 import "swiper/css";
 
-import { BsArrow90DegRight,BsGithub } from "react-icons/bs";
+import { BsArrowUpRight,BsGithub } from "react-icons/bs";
 
 
 import { Tooltip,TooltipProvider,TooltipContent,TooltipTrigger  } from "@/components/ui/tooltip";
@@ -16,41 +17,128 @@ import Image from "next/image";
 
 const projects=[
   {
-    num:'01',
+    num:"01",
     catogory:"frontend",
     title:'project 1',
     description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique quos ratione vero adipisci! Nihil at cum amet earum sequi molestiae voluptate sed voluptates laboriosam aliquam. Voluptate commodi archite",
     stack:[{name:"Next.js"},{name:"tailwind.css"},{name:"HTML5"}],
-    image:"/assets/work/thumb1.png",
+    image:"/assests/work/thumb1.png",
     live:"",
     github:"",
   },
   {
-    num:'02',
+    num:"02",
     catogory:"frontend",
     title:'project 2',
     description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique quos ratione vero adipisci! Nihil at cum amet earum sequi molestiae voluptate sed voluptates laboriosam aliquam. Voluptate commodi archite",
     stack:[{name:"Next.js"},{name:"tailwind.css"}],
-    image:"/assets/work/thumb2.png",
+    image:"/assests/work/thumb2.png",
     live:"",
     github:"",
   },{
-    num:'03',
+    num:"03",
     catogory:"frontend",
     title:'project 3',
     description:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt similique quos ratione vero adipisci! Nihil at cum amet earum sequi molestiae voluptate sed voluptates laboriosam aliquam. Voluptate commodi archite",
     stack:[{name:"Next.js"},{name:"tailwind.css"}],
-    image:"/assets/work/thumb3.png",
+    image:"/assests/work/thumb3.png",
     live:"",
     github:"",
   }
 ]
 
 const work = () => {
+  const [project,setProject]=useState(projects[0]);
+  const handleSlideChange = (swiper)=>{
+    const currentIndex=swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  }
+
+
   return (
-    <div>
-      work page
-    </div>
+    <motion.section  initial={{opacity:0}} animate={{opacity:1}} className="min-h-[80vh] flex 
+    flex-col justify-center py-12 xl:px-0  ">
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+          <div className="flex flex-col gap-[30px] h-[50%]">
+            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+              {project.num}
+            </div>
+            <h2 className="text-[42px] font-bold leading-none
+             text-white group-hover:text-accent transition-all 
+             duration-500 capitalize">
+              {project.catogory} project
+            </h2>
+            <p className="text-white/60 ">
+              {project.description}
+            </p>
+            <ul className="flex gap-4">
+              {project.stack.map((item,index)=>{
+                return(
+                  <li key={index} className="text-xl text-accent">
+                    {item.name}
+                    {index !== project.stack.length-1 && ","}
+
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="border border-white/20"></div>
+            <div className="flex items-center gap-4"> 
+              <Link href={project.live}>
+              <TooltipProvider delayDuration={100}>
+                <Tooltip>
+                  <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex items-center justify-center 
+                  group">
+                    <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Live project</p>
+                  </TooltipContent>
+                </Tooltip>
+
+              </TooltipProvider>
+              </Link>
+
+              <Link href={project.github}>
+              <TooltipProvider delayDuration={100}>
+                <Tooltip>
+                  <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex items-center justify-center 
+                  group">
+                    <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Github Repo</p>
+                  </TooltipContent>
+                </Tooltip>
+
+              </TooltipProvider>
+              </Link>
+
+
+            </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+              <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+                {projects.map((project,index)=>{
+                  return(<SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                        <div></div>
+                        <div className="relative w-full h-full">
+                        <Image src={project.image} fill className="object-cover" alt=""  />
+
+                        
+                    </div>
+                    </div>
+                  </SwiperSlide>)
+                })}
+              </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
   )
 }
 
